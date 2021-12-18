8f73f7eef8cbfc094203516ff41b03f7522fbc77
Balalaykajazz <archer2000@mail.ru>
2021-12-18 12:06:45 +0300

    add config

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -2,19 +2,27 @@ package main
 
 import (
 	"github.com/go-chi/chi"
+	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/http"
+	"workshop/internal/config"
 	"workshop/internal/handler"
 )
 
 func main() {
+	cfg := config.Server{}
+	err := cleanenv.ReadConfig("config.yml", &cfg)
+	if err != nil {
+		log.Fatal(err)
+	}
+
 	h := handler.NewHandler()
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
 
 	log.Println("Starting server")
-	err := http.ListenAndServe(":8081", r)
+	err = http.ListenAndServe(":8080", r)
 	//if err != nil {
 	log.Fatal(err)
 	//}
--- go.mod
+++ go.mod
@@ -2,4 +2,11 @@ module workshop
 
 go 1.17
 
-require github.com/go-chi/chi v1.5.4 // indirect
+require (
+	github.com/BurntSushi/toml v0.4.1 // indirect
+	github.com/go-chi/chi v1.5.4 // indirect
+	github.com/ilyakaznacheev/cleanenv v1.2.6 // indirect
+	github.com/joho/godotenv v1.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	olympos.io/encoding/edn v0.0.0-20201019073823-d3554ca0b0a3 // indirect
+)
