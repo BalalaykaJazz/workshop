adee10df00424ecdebb99706834d1e5628c3ece2
Balalaykajazz <archer2000@mail.ru>
2021-12-19 22:50:28 +0300

    add jokes

--- cmd/workshop/main.go
+++ cmd/workshop/main.go
@@ -5,6 +5,7 @@ import (
 	"github.com/ilyakaznacheev/cleanenv"
 	"log"
 	"net/http"
+	"workshop/internal/api/jokes"
 	"workshop/internal/config"
 	"workshop/internal/handler"
 )
@@ -16,12 +17,13 @@ func main() {
 		log.Fatal(err)
 	}
 
-	path := cfg.Host + ":" + cfg.Port
+	apiClient := jokes.NewJokeClient(cfg.JokeURL)
 
-	h := handler.NewHandler()
+	h := handler.NewHandler(apiClient)
 	r := chi.NewRouter()
 
 	r.Get("/hello", h.Hello)
+	path := cfg.Host + ":" + cfg.Port
 
 	log.Printf("starting server at %s", path)
 	err = http.ListenAndServe(path, r)
